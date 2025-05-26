bf007f4f3406de1b4b48f8beb2453daf44d41254
Temitope Olawuni <olawunitemitope68@gmail.com>
2025-05-26 11:41:10 +0100

    nodemon setup

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/userController";
+import { getUsers } from "../controllers/userController.ts";
 
 const router = Router();
 
