b5529cf21ab3815d20c6324867812f762c4c2db1
Temitope Olawuni <olawunitemitope68@gmail.com>
2025-05-26 09:42:28 +0100

    served client output to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,17 +4,21 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import cookieParser from "cookie-parser"
 /* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRout";
 import userRoutes from "./routes/userRoute";
 import expenseRoutes from "./routes/expenseRoute";
+import path from 'path'
 
 /* CONFIGURATIONS */
 dotenv.config();
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(helmet());
+app.use(cookieParser())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
@@ -27,6 +31,22 @@ app.use("/products", productRoutes); // http://localhost:8000/products
 app.use("/users", userRoutes); // http://localhost:8000/users
 app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
 
+
+ app.use(express.static(path.join(__dirname, '/client/out')));
+
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'out', 'index.html'));
+    })
+
+    app.use((err: { statusCode: number; message: string; },req:any,res:any,next:any)=>{
+        const statusCode = err.statusCode || 500;
+        const message = err.message || "Internal server error";
+        return res.status(statusCode).json({
+            success:false,
+            statusCode,
+            message,
+        })
+    })
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
